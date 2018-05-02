5dcc1ffdb3870cd7fd6f14e8e776177f35e78294
yanghy <yanghy@czfw.cn>
2018-05-02 14:21:18 +0800

    Initial Commit

---
+++ Test201805021421/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test201805021421
+//
+//  Created by 请加我杨先生 on 2018/5/2.
+//  Copyright © 2018年 请加我杨先生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test201805021421/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test201805021421
+//
+//  Created by 请加我杨先生 on 2018/5/2.
+//  Copyright © 2018年 请加我杨先生. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
